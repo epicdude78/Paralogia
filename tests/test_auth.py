bb76d25e1fffefa3346f08dca33d6dee71e86f13
epicdude78 <105817607+epicdude78@users.noreply.github.com>
2023-07-10 21:42:06 -0300

    Create test_auth.py

---
+++ tests/test_auth.py
@@ -0,0 +1,59 @@
+import sys
+import os
+
+sys.path.insert(0,(os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))))
+
+import pytest
+from flask import g
+from flask import session
+
+from paralogia.db import get_db
+
+
+def test_register(client, app):
+    # test that viewing the page renders without template errors
+    assert client.get("/auth/signup").status_code == 200
+
+    response = client.post("/auth/signup", data={"username": "aaaa", "password": "aaaabbbb2"})
+
+    # test that the user was inserted into the database
+    with app.app_context():
+        assert (
+            get_db().execute("SELECT * FROM user WHERE username = 'aaaa'").fetchone()
+            is not None
+        )
+
+
+
+
+def test_login(client, auth):
+    # test that viewing the page renders without template errors
+    assert client.get("/auth/login").status_code == 200
+
+    # test that successful login redirects to the index page
+    response = auth.login()
+    assert response.headers["Location"] == "/"
+
+    # login request set the user_id in the session
+    # check that the user is loaded from the session
+    with client:
+        client.get("/")
+        assert session["user_id"] == 1
+        assert g.user["username"] == "test"
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (("a", "test", b"Incorrect username or password."), ),
+)
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert "user_id" not in session
